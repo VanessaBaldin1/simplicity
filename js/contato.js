4342ed695eb231670a475c8b48d942b806d8e8da
VanessaBaldin1 <vanbaldin08@gmail.com>
2024-10-16 10:41:34 -0300

    Trabalhando com JQuery e Mask no formulário

--- js/contato.js
+++ js/contato.js
@@ -1,6 +1,9 @@
 /* Selecionando os elementos que serão manipulados */
 const formulario = document.querySelector("form");
 const campoCep = formulario.querySelector("#cep");
+// para incluir os dados do campo telefone/ precisa incluir o script no final da pagina do html
+const campoTelefone =formulario.querySelector("#telefone");
+
 const campoEndereco = formulario.querySelector("#endereco");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
@@ -8,9 +11,15 @@ const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
 const mensagemStatus = formulario.querySelector("#status");
 
+/* Ativação da máscara para telefone e CEP */
+$(campoTelefone).mask("(00) 0000-0000");
+$(campoCep).mask("00000-000");
+
+
+
 /* Capturando o clique no botão Buscar */
 
-botaoBuscar.addEventListener("click", function() {
+botaoBuscar.addEventListener("click", async function() {
     /* Verificando se o cep não tem 9 digitos */
 
     if ( campoCep.value.length !== 9) {
@@ -25,10 +34,67 @@ botaoBuscar.addEventListener("click", function() {
     //Guardando o valor do cep digitado
     let cepDigitado = campoCep.value;
    
+   
     
     /* AJAX - Asyncronous  Javascript And XML 
-    Técnica de comunicação (transmissão, recebimento) de dados muito usada entre sistemas e tecnologias diferentes*/
+    Técnica de comunicação assíncrona (transmissão, recebimento) de dados muito usada entre sistemas e tecnologias diferentes*/
     
+    //Etapa 1: Preparar a url contendo o cepa ser buscado
+    let url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+    console.log(url);
+
+
+    //Etapa 2: Acessar a API(com a url) e aguardar o retorno dela
+    const resposta = await fetch(url);
+    console.log(resposta);
+    
+    //Etapa 3: Extrair os dados do retorno/resposta
+    const dados = await resposta.json();
+    console.log(dados);
+    
+    //Etapa 4: lidar com os dados (em caso de erro e sucesso)
+    if("erro" in dados ){
+        mensagemStatus.innerHTML = "CEP inexistente 😢";
+        mensagemStatus.style.color = "red";
+    } else {
+        mensagemStatus.innerHTML = "CEP encontrado 😎";
+        mensagemStatus.style.color = "blue";
+
+        //Selecionando todos os campos com a classe indicada
+        const campos = formulario.querySelectorAll(".campos-restantes");
+
+        /* Loop/laço de Repetição para acessar CADA campo selecionadoe remover a classe fazendo com que cada campo volte a aparecer */
+
+
+        for(const campo of campos){
+            campo.classList.remove("campos-restantes");
+        }
+
+        /* Atribuir os dados para cada campo */
+
+        //Colocar o logradouro como valor do campo endereço
+
+        campoEndereco.value = dados.logradouro;
+
+
+        //Colocar o bairro como valor do campo bairro
+
+        campoBairro.value = dados.bairro;
+
+        //Colocar a localidade como valor do campo cidade
+
+        campoCidade.value = dados.localidade;
+
+        //Colocar a uf como valor do campo estado
+
+        campoEstado.value = dados.uf;
+
+
+
+    }
+   
+
+
 
 }); //final do evento do botão
 
