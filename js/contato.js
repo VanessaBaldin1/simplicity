66dddf2b6c2b4509333814979c5cff2e572aa163
VanessaBaldin1 <vanbaldin08@gmail.com>
2024-10-16 11:26:10 -0300

    Finalizamos a pagina contato com a inclusão do Formspree

--- js/contato.js
+++ js/contato.js
@@ -98,3 +98,34 @@ botaoBuscar.addEventListener("click", async function() {
 
 }); //final do evento do botão
 
+/* Codigo do Formspree */
+var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Obrigada! Mensagem enviada com sucesso. Aguarde nosso retorno.";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! Deu erro. Contate nosso suporte no e-mail contato@contato.com.br"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! Houve um erro.. fale com o Administrador pelo e-mail <a href=' mailto:admin@simplicity.com.br</a>"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
